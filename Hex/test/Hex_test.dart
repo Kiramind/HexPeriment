b9b0f98a8ee88eb622c46b999ffdeb75ae0e89d0
Mathias <mathias.aboudou@gmail.com>
2016-03-22 01:48:46 +0100

    first import commit

---
+++ Hex/test/Hex_test.dart
@@ -0,0 +1,18 @@
+// Copyright (c) 2015, <your name>. All rights reserved. Use of this source code
+// is governed by a BSD-style license that can be found in the LICENSE file.
+
+library Hex_test;
+
+import 'package:Hex/hex.dart';
+import 'package:unittest/unittest.dart';
+
+void main() => defineTests();
+
+void defineTests() {
+  //TODO test
+//  group('main tests', () {
+//    test('calculate', () {
+//      expect(calculate(), 42);
+//    });
+//  });
+}
